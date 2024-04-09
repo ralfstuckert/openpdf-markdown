2c5685426bdfc4c9be1af58ea07152f7b78651a8
Ralf Stuckert <ralf.stuckert@compeople.de>
2024-04-09 13:58:23 +0200

    add example

--- src/test/kotlin/com/github/ralfstuckert/openpdf/markdown/Example.kt
+++ src/test/kotlin/com/github/ralfstuckert/openpdf/markdown/Example.kt
@@ -1,8 +1,11 @@
 package com.github.ralfstuckert.openpdf.markdown
 
+import com.github.ralfstuckert.openpdf.markdown.renderer.StrikethroughMarkdownRenderer.Companion.STRIKETHROUGH_RENDER_CONTEXT_KEY
+import com.github.ralfstuckert.openpdf.markdown.renderer.TableMarkdownRenderer.Companion.TABLE_RENDER_CONTEXT_KEY
 import com.lowagie.text.Document
 import com.lowagie.text.pdf.PdfWriter
 import org.junit.jupiter.api.Test
+import java.awt.Color
 import java.io.FileOutputStream
 
 typealias Filename = String
@@ -15,27 +18,35 @@ class Example {
 # Tempor Invidunt
 Lorem ipsum dolor sit amet, **consetetur sadipscing** elitr, sed diam _nonumy eirmod tempor invidunt_ ut labore et dolore magna ~~aliquyam~~ erat, `sed diam voluptua`. [Nam liber tempor](https://github.com/ralfstuckert/openpdf-markdown/wiki/Getting-Started)
 
-| **Stet** | **Sanctus** |
-|:---:|-----------|
-| Clita   | `Stet clita` kasd> gubergren. |
-| Gubergren | Sea _takimata_ sanctus est Lorem |
+| **Stet** | **Sanctus** | **Nam liber** |
+|:---:|-----------|-------|
+| Clita   | `Stet clita` kasd gubergren. | Dolore magna |
+| Gubergren | Sea _takimata_ sanctus est Lorem ||
 
 - Stet
 - Clita
     1. Sanctus
     1. Lorem
     1. Dolor    
+>At vero _eos et accusam et justo_ 
+>duo dolores et ea rebum. 
 
-> At vero _eos et accusam et justo_ 
-> duo dolores et ea rebum. 
-
-sdfsdf
+Advancus curum adep.
         """.trimIndent()
         writeMarkdownPDF(markdown, "example.pdf")
     }
 
     fun writeMarkdownPDF(markdown:String, filename:Filename) {
-        val element = OpenPdfMarkdownGenerator().generate(markdown)
+        val markdownRendererRegistry = MarkdownRendererRegistry(defaultRenderContext).apply {
+            registerRenderContextFunction(TABLE_RENDER_CONTEXT_KEY, true) {
+                derive {
+                    this[PdfRenderContextKeys.COLSPAN_ENABLED] = true
+                    this[PdfRenderContextKeys.WEIGHTED_WIDTHS_ENABLED] = true
+                }
+            }
+        }
+
+        val element = OpenPdfMarkdownGenerator().generate(markdown, markdownRendererRegistry)
 
         with(FileOutputStream(filename)) {
             val document: Document = Document()
